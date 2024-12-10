79dc5c36285fc5e58de5e33ae94d80a953e48178
George Clark <george.clark@holidayextras.com>
2024-12-10 17:54:29 +0000

    clear

--- .tina/config.ts
+++
@@ -1,3 +0,0 @@
-import { defineConfig } from "tinacms";
-
-// Copy the same config from tina/config.ts here 
--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-
-export default defineConfig({
-  integrations: [react()],
-  output: 'static',
-  site: 'https://www.eastsussexaquapark.co.uk',  // Your custom domain
-  base: '/',  // Since we're using a custom domain, base should be '/'
-}); 
